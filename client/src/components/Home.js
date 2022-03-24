18545c095caa7a5b935a5ffa8565a336a84216f6
vladyslavhimself <vladyslavhimself@gmail.com>
2022-03-23 23:55:08 -0400

    fix state immutability

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -78,15 +78,20 @@ const Home = ({ user, logout }) => {
 
   const addNewConvo = useCallback((recipientId, message) => {
     
-    conversations.forEach((convo) => {
-      if (convo.otherUser.id === recipientId) {
-        convo.messages.push(message);
-        convo.latestMessageText = message.text;
-        convo.id = message.conversationId;
-      }
-    });
-    
-    setConversations([...conversations]);
+    setConversations((prev) => 
+      prev.map(convo => {
+        if (convo.otherUser.id === recipientId) {
+          const convoCopy = { ...convo };
+          convoCopy.messages = [...convoCopy.messages, message];
+          convoCopy.latestMessageText = message.text;
+          convoCopy.id = message.conversationId;
+          return convoCopy;
+        } else {
+          return convo;
+        }
+      })
+    )
+  
   }, [conversations, setConversations]);
 
   const addMessageToConversation = useCallback(
@@ -103,14 +108,18 @@ const Home = ({ user, logout }) => {
         setConversations((prev) => [newConvo, ...prev]);
         
       }
-      
-      conversations.forEach((convo) => {
-        if (convo.id === message.conversationId) {
-          convo.messages.push(message);
-          convo.latestMessageText = message.text;
-        }});
 
-      setConversations([...conversations]);
+      setConversations(prev => prev.map(convo => {
+        if (convo.id === message.conversationId) {
+          const convoCopy = { ...convo };
+          convoCopy.messages = [...convoCopy.messages, message];
+          convo.latestMessageText = message.text;
+          return convoCopy;
+        } else {
+          return convo;
+        }
+      }));
+      
     },[conversations, setConversations],
   );
 
