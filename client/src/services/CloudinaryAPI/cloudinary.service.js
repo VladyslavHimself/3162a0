809f3e0eea0efd78ffcc69bafaa09a148f7d9ba0
vladyslavhimself <vladyslavhimself@gmail.com>
2022-03-29 12:43:11 -0400

    add descriptions to service funcitons

--- client/src/services/CloudinaryAPI/cloudinary.service.js
+++ client/src/services/CloudinaryAPI/cloudinary.service.js
@@ -13,7 +13,11 @@ export class CloudinaryService {
 
     defaultAxiosInstance = axios.create();
 
-
+    /*
+    *   @description - Generates data in form structure, send data to cloudinary, and returns Promise pending promise.
+    *   @params {string} -  image to upload.
+    *   @returns {Promise} - Promise with image data.
+    */
     _sendImageToServer = (image) => {
         const formData = new FormData();
         formData.append('file', image);
@@ -22,12 +26,21 @@ export class CloudinaryService {
         return this.defaultAxiosInstance.post(this._apiLink, formData);
     };
 
+    /*
+    *   @description - Collect all pendings image promises and push it to selected array variable.
+    *   @params {Array} - Array, where we want to put all pending promises.
+    *   @returns {Void} - There is no returns.
+    */
     _collectAllImagePromises = (_imageDataPromise) => {
         [...this._selectedImages].forEach(selectedImage => {
             _imageDataPromise.push(this._sendImageToServer(selectedImage));
         });
     }
 
+    /*
+    *   @description - function with incapsulated logic. Takes data from constructor, post images to cloud & receive data.
+    *   @returns {Void} - all pending image promises. The are only avaliable, if all promises received.
+    */
     postImageAndReceiveData = () => {
         const imageDataPromises = [];
         this._collectAllImagePromises(imageDataPromises);
