0e919d155d4bae2f800e4d122900ef8e1dfea449
vladyslavhimself <vladyslavhimself@gmail.com>
2022-03-30 23:29:00 -0400

    configured initial components for using message component

--- client/src/components/ActiveChat/OtherUserBubble.js
+++ client/src/components/ActiveChat/OtherUserBubble.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Box, Typography, Avatar } from '@material-ui/core';
+import MessageContent from "../MessageContent";
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -21,6 +22,7 @@ const useStyles = makeStyles(() => ({
   bubble: {
     backgroundImage: 'linear-gradient(225deg, #6CC1FF 0%, #3A8DFF 100%)',
     borderRadius: '0 10px 10px 10px',
+    padding: '10px',
   },
   text: {
     fontSize: 14,
@@ -45,9 +47,7 @@ const OtherUserBubble = ({ text, time, otherUser, images }) => {
         <Typography className={classes.usernameDate}>
           {otherUser.username} {time}
         </Typography>
-        <Box className={classes.bubble}>
-          <Typography className={classes.text}>{text}</Typography>
-        </Box>
+        <MessageContent images={images} text={text} componentStyles={useStyles} />
       </Box>
     </Box>
   );
--- client/src/components/ActiveChat/SenderBubble.js
+++ client/src/components/ActiveChat/SenderBubble.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Box, Typography } from '@material-ui/core';
+import MessageContent from "../MessageContent";
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -14,6 +15,7 @@ const useStyles = makeStyles(() => ({
     fontWeight: 'bold',
     marginBottom: 5,
   },
+
   text: {
     fontSize: 14,
     color: '#91A3C0',
@@ -21,48 +23,21 @@ const useStyles = makeStyles(() => ({
     padding: 8,
     fontWeight: 'bold',
   },
+
   bubble: {
     background: '#F4F6FA',
     borderRadius: '10px 10px 0 10px',
-  },
+  }
 }));
 
-const useImageStyles = (gallerySize, text) => makeStyles(() => ({
-  image: {
-    maxWidth:
-        gallerySize.length > 1 ? '128px' : '256px',
-    maxHeight:
-        gallerySize.length > 1 ? '128px' : '256px',
-
-    backgroundSize: 'cover',
-    borderTopRightRadius: '10px',
-    borderTopLeftRadius: '10px',
-    borderRadius: !text && '10px',
-    borderBottomRightRadius: !text && '0px',
-
-    transition: '.5s ease',
-    '&:hover': {
-      filter: 'brightness(0.8)',
-    }
-  },
-}));
 
 const SenderBubble = ({ time, text, images }) => {
-  const classes = useStyles();
-  const imgCls = useImageStyles(images, text)();
+  const { root, date } = useStyles();
+
   return (
-    <Box className={classes.root}>
-      <Typography className={classes.date}>{time}</Typography>
-      <Box className={classes.bubble}>
-      {
-        images && images.map((image, idx) => (
-          <a key={`image{${idx}`} href={image}>
-            <img className={imgCls.image} src={image} alt={`img with description ${text}`} />
-          </a>
-        ))
-      }
-      { text && <Typography className={classes.text}>{text}</Typography> }
-      </Box>
+    <Box className={root}>
+      <Typography className={date}>{time}</Typography>
+      <MessageContent images={images} text={text} isAccountSender componentStyles={useStyles} />
     </Box>
   );
 };
